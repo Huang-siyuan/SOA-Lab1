d7e2c3c8a4b936f071ad4d10482feee92a034d49
Huang-siyuan <975892929@qq.com>
2022-05-28 17:51:37 +0300

    Ini more. Remember if you use Tomcat, the version should be 9.0.X.

---
+++ src/main/java/org/itmo/ActionTest.java
@@ -0,0 +1,30 @@
+package org.itmo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @program: SOA-Lab1
+ * @author: Siyuan
+ * @create: 2022-05-28 15:24
+ **/
+
+@WebServlet(name = "ActionTest", urlPatterns = "/ActionTest")
+public class ActionTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("do Get");
+        resp.getWriter().write("<h1>Hello World</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("do Post");
+        resp.getWriter().write("<h1>Hello World</h1>");
+    }
+}
